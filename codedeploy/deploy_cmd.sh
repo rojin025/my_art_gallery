d707a7ce533426cd1f86687773e07359c7c2282e
Rojin Bijukchhe <ro_jin25@icloud.com>
2025-02-19 19:52:08 -0600

    deployment ready.

--- codedeploy/deploy_cmd.sh
+++ codedeploy/deploy_cmd.sh
@@ -4,4 +4,4 @@ aws deploy create-deployment \
  --deployment-config-name CodeDeployDefault.OneAtATime \
  --deployment-group-name art_gallery_deployment_group \
  --description "Demo Deployment" \
- --github-location repository=brandon-rich-aws-deployment-course/my_art_gallery,commitId=$(git rev-parse HEAD) \
+ --github-location repository=rojin025/my_art_gallery,commitId=$(git rev-parse HEAD) \
